bc70bb920fc675b18000d6c45fde8f9f40c36ed3
Vinoth0109 <118457180+Vinoth0109@users.noreply.github.com>
2022-11-23 14:52:24 +0100

    modif files

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -3,6 +3,7 @@
 <template>
     <div id="main-footer">
         <!-- <RouterLink v-bind:to="{name:'contact-view'}">Contact</RouterLink> -->
+        <h3>Ici c'est le footer</h3>
     </div>
 </template>
 
