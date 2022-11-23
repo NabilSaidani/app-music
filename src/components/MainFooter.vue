6506e6ae53a2e21f39c8095514d077b3b29edcff
Vinoth0109 <118457180+Vinoth0109@users.noreply.github.com>
2022-11-23 10:38:28 +0100

    create MainFooter.vue

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+<template>
+    <div id="main-footer">
+        <!-- <RouterLink v-bind:to="{name:'contact-view'}">Contact</RouterLink> -->
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,10 @@
 <script>
 
+
 </script>
 
 <template>
-    
+    <h1>navibl</h1>
 </template>
 
 <style scoped>
