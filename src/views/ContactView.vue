dd45c0cc1476daae04211bd5e6ceebe116b99d2f
NabilSaidani <96593015+NabilSaidani@users.noreply.github.com>
2022-11-23 11:52:24 +0100

    Update ContactView.vue
    J'ai fini le formulaire

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,30 @@
 <script></script>
 
 <template>
-  <h1>navibl</h1>
+  <h1>Formulaire :</h1>
+  <div id="main-content">
+    <form action="#" method="post">
+      <p>
+        <label for="nom">Nom</label>
+        <input id="nom" type="text" name="nom" />
+      </p>
+      <p>
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" id="prenom" />
+      </p>
+      <p>
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </p>
+      <p>
+        <label for="message">Message :</label>
+        <textarea name="message" id="user_message" cols="30" rows="10"></textarea>
+      </p>
+      <p class="button">
+        <button type="submit">Envoyer le message</button>
+      </p>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
