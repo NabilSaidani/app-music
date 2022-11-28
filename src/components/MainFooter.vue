003233b145d87c62d8cc58d04d56027c62007027
NabilSaidani <96593015+NabilSaidani@users.noreply.github.com>
2022-11-28 13:20:50 +0100

    MainFooter.vue et PlaylistView.vue
    J'ai fait quelque modif

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,12 +1,24 @@
-<script>
-</script>
+<script></script>
 <template>
-    <div id="main-footer">
-        <!-- <RouterLink v-bind:to="{name:'contact-view'}">Contact</RouterLink> -->
-        <h3>Ici c'est le footer</h3>
+  <h3>Ici c'est le footer</h3>
+  <div id="main-footer">
+    <div class="link">
+      <a
+        href="https://www.deezer.com/fr/?utm_campaign=acq_fr_perfmax_web_all_perf_tnb-directsub&utm_content=alloffers&utm_medium=display_&utm_source=adwords&utm_term=premium-offline&gclid=Cj0KCQiA1ZGcBhCoARIsAGQ0kkrzHe2mbBhGnPM449XjHqDkc0CDg60XC2T88ir0MpMTeSwmIgvbI7kaAiT8EALw_wcB"
+      ></a>
+      <ul class="list-social">
+        <li class="social-list-item">
+          <img src="" alt="" />
+          <a href="https://www.facebook.com/deezerfr"></a>
+        </li>
+      </ul>
     </div>
+    <!-- <RouterLink v-bind:to="{name:'contact-view'}">Contact</RouterLink> -->
+  </div>
 </template>
 
-<style>
-
+<style scoped>
+#main-footer {
+  background-color: #333;
+}
 </style>
--- src/views/PlaylistView.vue
+++ src/views/PlaylistView.vue
@@ -5,32 +5,32 @@ export default {
       sonlistMoment: [
         {
           name: "Hit 2022",
-        //   description: "Appareil d'écoute radiophonique ou téléphonique.",
+          //   description: "Appareil d'écoute radiophonique ou téléphonique.",
           image: "casque.jpeg",
         },
         {
           name: "Rock 90s",
-        //   description: "C'est une banane jaune qui est importé du brésil.",
+          //   description: "C'est une banane jaune qui est importé du brésil.",
           image: "banane.jpeg",
         },
         {
           name: "Rap Us",
-        //   description: "Pour allumer le feu.",
+          //   description: "Pour allumer le feu.",
           image: "briquet.jpeg",
         },
         {
           name: "Hellfest",
-        //   description: "Pour allumer le feu.",
+          //   description: "Pour allumer le feu.",
           image: "briquet.jpeg",
         },
         {
           name: "Noël",
-        //   description: "Pour allumer le feu.",
+          //   description: "Pour allumer le feu.",
           image: "briquet.jpeg",
         },
         {
           name: "Classique",
-        //   description: "Pour allumer le feu.",
+          //   description: "Pour allumer le feu.",
           image: "briquet.jpeg",
         },
       ],
