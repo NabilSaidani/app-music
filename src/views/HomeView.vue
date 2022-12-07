d3ad47bf932aebbdbb8c565901be6b10d9fa1a91
NabilSaidani <96593015+NabilSaidani@users.noreply.github.com>
2022-12-07 13:00:30 +0100

    HomeView.vue
    J'ai ajouté un paragraphe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,14 @@
 
 <template>
     <h1>Bienvenue</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Pellentesque sit amet sapien hendrerit, mattis mi sed,
+lacinia neque. Mauris non ipsum non magna
+consectetur sollicitudin. Suspendisse vestibulum
+rhoncus erat in pretium. Quisque euismod sem eget
+ante sagittis, vitae tincidunt purus commodo. In ut mi
+eget nisi imperdiet facilisis.
+</p>
 </template>
 
 <style scoped></style>
