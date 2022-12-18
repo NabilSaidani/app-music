00f658012e0a0c23d4dcffb00a3e179f9ccf8886
NabilSaidani <96593015+NabilSaidani@users.noreply.github.com>
2022-12-19 00:50:19 +0100

    summary required
    J'ai ajouté une vieu detailsplayliste
    j'ai commencé a faire le footer, demain je vais faire fonctionner les appel api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,27 @@
 <script></script>
 
 <template>
+  <div class="home">
     <h1>Bienvenue</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Pellentesque sit amet sapien hendrerit, mattis mi sed,
-lacinia neque. Mauris non ipsum non magna
-consectetur sollicitudin. Suspendisse vestibulum
-rhoncus erat in pretium. Quisque euismod sem eget
-ante sagittis, vitae tincidunt purus commodo. In ut mi
-eget nisi imperdiet facilisis.
-</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit
+      amet sapien hendrerit, mattis mi sed, lacinia neque. Mauris non ipsum non
+      magna consectetur sollicitudin. Suspendisse vestibulum rhoncus erat in
+      pretium. Quisque euismod sem eget ante sagittis, vitae tincidunt purus
+      commodo. In ut mi eget nisi imperdiet facilisis.
+    </p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1{
+    text-align: center;
+    color: white;
+    background-color: #333;
+}
+p {
+  text-align: center;
+  color: white;
+  background-color: #333;
+}
+</style>
--- src/views/PlaylistView.vue
+++ src/views/PlaylistView.vue
@@ -1,5 +1,5 @@
 <script>
-import MusicBlock from '@/components/MusicBlock.vue';
+import MusicBlock from "@/components/MusicBlock.vue";
 
 export default {
   components: { MusicBlock },
@@ -7,41 +7,38 @@ export default {
     return {
       title: "Playlist",
       productList: [],
+    };
+  },
+  async mounted() {
+    //chargement de la list des produits depuis l'API
+    let requestUrl =
+      "https://sbtem2f2il.execute-api.eu-west-1.amazonaws.com/default/deezerProxyApi?playlist=699296985";
+    let requestOptions = {
+      method: "GET",
+    };
+
+    const response = await fetch(requestUrl, requestOptions);
+    const jsonContent = await response.json();
+
+    let messageErreur = "";
+    if (response.status == "400") {
+      this.messageErreur = "Impossible de charger l'url API";
+    }
+
+    console.log(response.status);
+    for (let productFromApi of jsonContent["data"]) {
+      console.log("cover", productFromApi.cover);
+      this.productList.push({
+        name: productFromApi.title,
+        image: productFromApi.cover,
+      });
     }
   },
-  async mounted(){
-        //chargement de la list des produits depuis l'API
-        let requestUrl = "https://sbtem2f2il.execute-api.eu-west-1.amazonaws.com/default/deezerProxyApi?playlist=699296985"
-        let requestOptions = {
-          method: 'GET',
-        }
-
-        const response = await fetch(requestUrl, requestOptions);
-        const jsonContent = await response.json();
-
-        let messageErreur = ""; 
-        if(response.status == '400'){
-          this.messageErreur = "Impossible de charger l'url API";           
-        }
-
-        console.log(response.status);
-        for(let productFromApi of jsonContent['data']){
-          console.log('cover',productFromApi.cover)
-          this.productList.push({
-            name:productFromApi.title,
-            image: productFromApi.cover,
-          })
-        }
-      }
-
 };
 </script>
 
 <template>
   <h1>Playlist</h1>
-  
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
